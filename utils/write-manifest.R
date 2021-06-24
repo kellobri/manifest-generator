cfd1316b65f8e42f59989fdecddff035c7e731ca
kellobri <kellyobriant@gmail.com>
2021-06-23 19:12:58 -0700

    test env vars

--- utils/write-manifest.R
+++ utils/write-manifest.R
@@ -2,13 +2,13 @@
 
 options(repos = c(REPO_NAME = "https://packagemanager.rstudio.com/all/__linux__/focal/latest"))
 
-#content_bundle <- paste0("bundles-", Sys.getenv("R_VERSION"))
-content_bundle <- paste0("bundles-", "${{ env.R_VERSION }}")
+content_bundle <- paste0("bundles-", Sys.getenv("R_VERSION"))
+#content_bundle <- paste0("bundles-", "${{ env.R_VERSION }}")
 print(content_bundle)
 
 dir.create(content_bundle, showWarnings = FALSE)
-#file.copy(Sys.getenv("CONTENT_PATH"), content_bundle)
-file.copy("${{ env.CONTENT_PATH }}", content_bundle)
+file.copy(Sys.getenv("CONTENT_PATH"), content_bundle)
+#file.copy("${{ env.CONTENT_PATH }}", content_bundle)
 
 list.files(content_bundle)
 
