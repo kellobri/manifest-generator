e980e795f3bfecd36b04b61cc3c513a184bab82e
kellobri <kellyobriant@gmail.com>
2021-06-23 19:03:56 -0700

    Add prints

--- utils/write-manifest.R
+++ utils/write-manifest.R
@@ -3,9 +3,12 @@
 options(repos = c(REPO_NAME = "https://packagemanager.rstudio.com/all/__linux__/focal/latest"))
 
 content_bundle <- paste0("bundles-", Sys.getenv("R_VERSION"))
+print(content_bundle)
 
 dir.create(content_bundle, showWarnings = FALSE)
 file.copy(Sys.getenv("CONTENT_PATH"), content_bundle)
+
+list.files(content_bundle)
 
 install.packages('rsconnect')
 
