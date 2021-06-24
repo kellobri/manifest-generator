3856099af26aeef44837dde631f564b68ffe2730
kellobri <kellyobriant@gmail.com>
2021-06-23 18:48:00 -0700

    Additional steps

--- utils/install-r.sh
+++ utils/install-r.sh
@@ -4,11 +4,14 @@
 #
 
 sudo apt-get update
-sudo apt-get install gdebi-core
+sudo apt-get -y install gdebi-core
+sudo apt-get install -y libcurl4-openssl-dev
+sudo apt-get install -y libssl-dev
 
 cd /opt && curl -O https://cdn.rstudio.com/r/ubuntu-2004/pkgs/r-${R_VERSION}_1_amd64.deb
-cd /opt && sudo gdebi r-${R_VERSION}_1_amd64.deb
+cd /opt && sudo gdebi -n r-${R_VERSION}_1_amd64.deb
 
 sudo ln -s /opt/R/${R_VERSION}/bin/R /usr/local/bin/R
 sudo ln -s /opt/R/${R_VERSION}/bin/Rscript /usr/local/bin/Rscript
 
+/opt/R/${R_VERSION}/bin/R --version
