a454ddd3fe2211c80a8321f7381a9170079076dc
kellobri <kellyobriant@gmail.com>
2021-06-23 18:55:10 -0700

    revert env vars

--- utils/write-manifest.R
+++ utils/write-manifest.R
@@ -2,10 +2,10 @@
 
 options(repos = c(REPO_NAME = "https://packagemanager.rstudio.com/all/__linux__/focal/latest"))
 
-content_bundle <- paste0("bundles-","${{ env.R_VERSION }}")
+content_bundle <- paste0("bundles-", Sys.getenv("R_VERSION"))
 
 dir.create(content_bundle, showWarnings = FALSE)
-file.copy("${{ env.CONTENT_PATH }}", content_bundle)
+file.copy(Sys.getenv("CONTENT_PATH"), content_bundle)
 
 install.packages('rsconnect')
 
