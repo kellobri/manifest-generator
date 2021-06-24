3bc777d9798d5dd3056b1ec33957f694fd20ac1f
kellobri <kellyobriant@gmail.com>
2021-06-23 18:39:27 -0700

    remove typo

--- utils/write-manifest.R
+++ utils/write-manifest.R
@@ -1,6 +1,6 @@
 #!/usr/bin/env Rscript
 
-content_bundle <- paste0("bundles-","${{ env.R_VERSION }}"))
+content_bundle <- paste0("bundles-","${{ env.R_VERSION }}")
 
 dir.create(content_bundle, showWarnings = FALSE)
 file.copy("${{ env.CONTENT_PATH }}", content_bundle)
